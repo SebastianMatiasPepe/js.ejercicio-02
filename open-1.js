252f4d395e434c80b4a2939da3e06a281bb285c6
Sebatian Matias Pepe <110673120+SebastianMatiasPepe@users.noreply.github.com>
2023-03-04 11:28:16 -0300

    Add files via upload

---
+++ open-1.js
@@ -0,0 +1,13 @@
+const libro_favorito ={
+    titulo : "El resplandor" ,
+    autor : "stephen King" ,
+    fecha :" 28/01/1977",
+    url : "https://www.mercadolibre.com.ar/libro-el-resplandor-bolsillo-stephen-king/p/MLA21017746?pdp_filters=category:MLA412445#searchVariation=MLA21017746&position=1&search_layout=stack&type=product&tracking_id=534f86fd-e277-41ba-abb8-f941a6556f17",
+}
+
+
+const datos = ["Sebastian , 32 , True , 14/03/1990 ,libro Favorito: " , libro_favorito] ;
+
+console.log(datos); 
+
+
